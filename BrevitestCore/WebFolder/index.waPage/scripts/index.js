52b0e97b8c8a0d572aaeb7d20a3fb3ab4b0cc600
Leo Linbeck III <leo3@linbeck.com>
2014-10-19 19:02:20 -0500

    Index page and serverControl RPC

---
+++ BrevitestCore/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,27 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var buttonLoadShamData = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	buttonLoadShamData.click = function buttonLoadShamData_click (event)// @startlock
+	{// @endlock
+		serverControl.loadShamData({
+			onSuccess: function(evt) {
+				debugger;
+				console.log('Result: ', evt);
+			},
+			onError: function(err) {
+				debugger;
+				console.log('Result: ', err);
+			}
+		});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("buttonLoadShamData", "click", buttonLoadShamData.click, "WAF");
+// @endregion
+};// @endlock
