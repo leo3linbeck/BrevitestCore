42451ebc29c3604c27ddbf21afab39e000443a8e
Leo Linbeck III <leo3@linbeck.com>
2015-02-08 10:20:37 -0600

    Refactoring for Spark communication

---
+++ BrevitestCore/SSJS Utilities/SparkCore test 2.js
@@ -0,0 +1,1 @@
+﻿var spark = require('sparkCore');var sparkCoreID = '53ff6e066667574844262367';var accessToken = spark.getAccessToken(sparkCoreID);var status = spark.readSerialNumber(sparkCoreID);(status);
